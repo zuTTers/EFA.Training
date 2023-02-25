0a785810f22256bf99218bb08c74ceb4fd85d01e
Zübeyir Koçalioğlu <zubeyir.kocalioglu@gmail.com>
2023-02-25 17:58:38 +0300

    builded

--- EFA/wwwroot/app/src/environments/environment.prod.ts
+++ EFA/wwwroot/app/src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ import { config } from "config";
 
 export const environment = {
     production: true,
-    apiURL: 'http://192.168.1.102:91/api',
-    imagePath: 'http://192.168.1.102:91/',
+    apiURL: 'https://192.168.1.102:91/api',
+    imagePath: 'https://192.168.1.102:91/',
     loginPage: config.loginPage
 };
 //export const environment = {
 //  production: true,
-//    apiURL: 'http://192.168.1.180:93/api',
-//    imagePath: 'http://192.168.1.180:93/',
+//    apiURL: 'https://192.168.1.180:93/api',
+//    imagePath: 'https://192.168.1.180:93/',
 //    loginPage: config.loginPage
 //};
